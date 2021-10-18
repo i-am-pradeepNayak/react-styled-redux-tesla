457bc5ec4d80320d9f91b1d3b8750398fbb870b0
pradeep <nayakpradeep47@gmail.com>
2021-10-18 10:27:28 +0530

    project initialized

---
+++ src/Components/Section.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: orange;
+`;
+
+const Section = () => {
+  return <Wrap>section</Wrap>;
+};
+
+export default Section;
