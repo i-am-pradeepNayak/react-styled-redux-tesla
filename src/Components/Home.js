6e520f4bd0bf78837be6355b2b04720b03c292dd
pradeep <nayakpradeep47@gmail.com>
2021-10-18 15:41:45 +0530

    Section component added and styled props

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -9,7 +9,50 @@ const Container = styled.div`
 const Home = () => {
   return (
     <Container>
-      <Section />
+      <Section
+        title={'Model s'}
+        description="order inline for touchless delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title={'Model x'}
+        description="order inline for touchless delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title={'Model y'}
+        description="order inline for touchless delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+
+      <Section
+        title="lowest cost solar"
+        description="order inline for touchless delivery"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+
+      <Section
+        title="Solar for new roofs"
+        description="order inline for touchless delivery"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+
+      <Section
+        title="Accessories"
+        description="order inline for touchless delivery"
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop Now"
+      />
     </Container>
   );
 };
--- src/Components/Section.js
+++ src/Components/Section.js
@@ -4,11 +4,82 @@ import styled from 'styled-components';
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url('/public/images/model-s.jpg');
+  background-image: ${props => `url("/images/${props.backgroundImg}")`};
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
-const Section = () => {
-  return <Wrap>section</Wrap>;
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  color: white;
+  width: 256px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  font-size: 12px;
+  text-transform: uppercase;
+  opacity: 0.85;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background: white;
+  color: black;
+  opacity: 0.65;
+`;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
+  animation: animationDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div``;
+
+const Section = ({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText
+}) => {
+  return (
+    <Wrap backgroundImg={backgroundImg}>
+      <ItemText>
+        <h1> {title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
 };
 
 export default Section;
